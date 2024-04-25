df7cb2c66a1b21b31e0bdecdbcff6d36c89649d4
quiller <guilhermequillerdealencar@gmail.com>
2024-04-24 21:49:13 -0300

    feat(rw-gui): Implementa TelaHome
    Former-commit-id: 5f7046985c29e68e500cef915f35c983c4d19e9b

---
+++ src/main/java/RW/Drawer/MyDrawerBuilder.java
@@ -0,0 +1,86 @@
+
+package RW.Drawer;
+
+import java.util.HashSet;
+import java.util.Set;
+import raven.drawer.component.SimpleDrawerBuilder;
+import raven.drawer.component.footer.SimpleFooterData;
+import raven.drawer.component.header.SimpleHeaderData;
+import raven.drawer.component.menu.MenuAction;
+import raven.drawer.component.menu.MenuEvent;
+import raven.drawer.component.menu.MenuValidation;
+import raven.drawer.component.menu.SimpleMenuOption;
+import raven.swing.AvatarIcon;
+import javax.swing.JMenu;
+
+
+/**
+ *
+ * @author Guilherme Quiller
+ */
+public class MyDrawerBuilder extends SimpleDrawerBuilder{
+
+    @Override
+    public SimpleHeaderData getSimpleHeaderData() {
+        return new SimpleHeaderData()
+                //adicionar a foto para mudar de acordo o o usurio
+                .setIcon(new AvatarIcon(getClass().getResource("/imagens/Reforest World Logo.png"),60,60, 999))
+                //adicionar o nome da pessoa conforme ela logar, puxando do banco de dados
+                .setTitle("nome do usuario")
+                //adicionar o e-mail da pessoa que entrar
+                .setDescription("o e-mail da pessoa");
+        
+        
+    }
+
+    @Override
+    public SimpleMenuOption getSimpleMenuOption() {
+        String menus[][] = {
+            {"~MAIN~"},
+            {"Dashboard"},
+            {"~WEB APP~"},
+            {"Email", "Inbox", "Read", "Compost"},
+            {"Chat"},
+            {"Calendar"},
+            {"~COMPONENT~"},
+            {"Advanced UI", "Cropper", "Owl Carousel", "Sweet Alert"},
+            {"Forms", "Basic Elements", "Advanced Elements", "SEditors", "Wizard"},
+            {"~OTHER~"},
+            {"Charts", "Apex", "Flot", "Sparkline"},
+            {"Icons", "Feather Icons", "Flag Icons", "Mdi Icons"},
+            {"Special Pages", "Blank page", "Faq", "Invoice", "Profile", "Pricing", "Timeline"},
+            {"Logout"}
+        };
+        
+        String icons[] = {
+            "dashboard.svg",
+            "email.svg",
+            "chat.svg",
+            "calendar.svg",
+            "ui.svg",
+            "forms.svg",
+            "chart.svg",
+            "icon.svg",
+            "page.svg",
+            "logout.svg"};
+         
+        return new SimpleMenuOption()
+               .setMenus(menus)
+               .setIcons(icons)
+               .setBaseIconPath("RW/Drawer/icon")
+               .setIconScale(0.45f)
+               .addMenuEvent(new MenuEvent() {
+                    public void selected(MenuAction action, int index, int subIndex) {
+                        System.err.println("Menu selected "+index+" "+subIndex);
+                    }
+                })
+                
+        ;
+    }
+
+    @Override
+    public SimpleFooterData getSimpleFooterData() {
+        return new SimpleFooterData();
+    }
+    
+}
