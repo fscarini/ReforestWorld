b61bafcc9ebe84b7c81a071401e2ebceceb38051
quiller <guilhermequillerdealencar@gmail.com>
2024-04-25 21:18:44 -0300

    feat(RW-gui); Implementa barra de tarefas
    Former-commit-id: 09e83e7364120805291e432e33e5f4951da9af5f

--- src/main/java/RW/forms/TelaHome.java
+++ src/main/java/RW/forms/TelaHome.java
@@ -4,7 +4,9 @@
  */
 package RW.forms;
 
+
 import RW.Drawer.MyDrawerBuilder;
+import RW.Tabbed.WindowsTabbed;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
@@ -26,47 +28,33 @@ public class TelaHome extends javax.swing.JFrame {
         MyDrawerBuilder myDrawerbuilder=new MyDrawerBuilder();
         Drawer.getInstance().setDrawerBuilder(myDrawerbuilder);
         initComponents();
+        WindowsTabbed.getInstance().install(this,body);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        body = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("mostrar abas");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        body.setLayout(new java.awt.BorderLayout());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(670, Short.MAX_VALUE))
+            .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, 794, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(424, Short.MAX_VALUE))
+            .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        Drawer.getInstance().showDrawer();
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -74,17 +62,17 @@ public class TelaHome extends javax.swing.JFrame {
     
     public static void main(String args[]) {
         FlatRobotoFont.install();
-        FlatLaf.registerCustomDefaultsSource("tema.floresta");
-        UIManager.put("FontePadrao", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        FlatLaf.registerCustomDefaultsSource("RW.themes");
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
         FlatMacDarkLaf.setup();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaHome().setVisible(true);
+                new TelaHome().setVisible(true);    
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel body;
     // End of variables declaration//GEN-END:variables
 }
