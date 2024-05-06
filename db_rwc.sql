22d777b146d80634bce4c3656cdbd7f43a6b8f59
Raul Santos <164427684+raulzets@users.noreply.github.com>
2024-05-05 22:57:46 -0300

    feat(rw-db): cria a tabela eventos

--- db_rwc.sql
+++ db_rwc.sql
@@ -9,8 +9,16 @@ primary key (id)
 );
 insert into users(id, nome, email, senha) values (null, 'admin', 'admin@reforestworld,com.br','admin');
 select * from users;
+select * from eventos;
 update users set cpf= '123.456.789-92' where id = 55;
 ALTER TABLE `db_rwc`.`users` 
 ADD COLUMN `dt_nascimento` VARCHAR(10) NOT NULL AFTER `senha`,
 ADD COLUMN `sexo` VARCHAR(1) NOT NULL AFTER `dt_nascimento`,
 ADD COLUMN `cpf` VARCHAR(11) NOT NULL AFTER `sexo`;
+ALTER TABLE `db_rwc`.`eventos` 
+ADD COLUMN `local` VARCHAR(250) NOT NULL AFTER `nome`,
+ADD COLUMN `data` DATETIME NOT NULL AFTER `local`,
+ADD COLUMN `id_usuario` INT NOT NULL AFTER `descricao`,
+CHANGE COLUMN `nome do evento` `nome` VARCHAR(250) NOT NULL ,
+CHANGE COLUMN `data do evento` `descricao` VARCHAR(10) NOT NULL ,
+CHANGE COLUMN `data do registro` `data_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;
