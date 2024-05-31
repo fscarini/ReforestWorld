542b07c30c7c45e43f5398fbc9460c9adf4502c9
Raul Santos <164427684+raulzets@users.noreply.github.com>
2024-05-31 01:39:17 -0300

    feat(rw-gui): remove metodo main das telas de load

--- src/main/java/RW/forms/LoginLoadTela.java
+++ src/main/java/RW/forms/LoginLoadTela.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package RW.forms;
 
 import java.awt.Color;
 import javax.swing.JDialog;
 
-/**
- *
- * @author RAVEN
- */
 public class LoginLoadTela extends javax.swing.JDialog {
 
-    /**
-     * Creates new form SplashScreen
-     */
     public LoginLoadTela(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
@@ -124,49 +112,6 @@ public class LoginLoadTela extends javax.swing.JDialog {
         lbStatus.setText(taskName);
         Thread.sleep(1000); //  For Test
         pro.setValue(progress);
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginLoadTela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginLoadTela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginLoadTela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginLoadTela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                LoginLoadTela dialog = new LoginLoadTela(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
